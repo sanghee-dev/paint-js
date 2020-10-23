deca55390d254e0c95bed66790888349429e081f
leeedev <leeesangheee@gmail.com>
2020-10-23 18:10:31 +0900

    Add canvas painting

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
-const canvas = document.querySelector("#jsCanvas");
+const canvas = document.querySelector("#jsCanvas"),
+  ctx = canvas.getContext("2d"),
+  vh = window.innerHeight / 100;
+
+canvas.width = 92 * vh;
+canvas.height = 92 * vh;
+
+ctx.lineWidth = 1;
+ctx.strokeStyle = "#2c2c2c";
 
 let painting = false;
 
 function stopPainting() {
   painting = false;
-  console.log("stop painting!");
+}
+
+function startPainting() {
+  painting = true;
 }
 
 function onMouseMove(event) {
   const x = event.offsetX;
   const y = event.offsetY;
-}
-
-function onMouseDown(event) {
-  painting = true;
-  console.log(painting);
-}
-function onMouseUp(event) {
-  stopPainting();
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
